e7d0dd13640033a9e079be87bb31b8e966340f3f
Nathanael Adam <nathanaeladam3@gmail.com>
2021-05-10 01:45:40 +0200

    nathanael commit the Navbar in Index.html and Function Init() and generateNavbar()

--- script/overall.js
+++ script/overall.js
@@ -11,3 +11,19 @@ function getAllTasks() {
 function addTask(task) {
     tasksDb.push(task);
 }
+
+function init() {
+    generateNavbar();
+}
+
+function generateNavbar() {
+    document.getElementById('nav-bar').classList.add('bg-sec'); // add the background color
+    document.getElementById('nav-bar').innerHTML = `
+    <a href="#"><img class="nav-logo" src="./img/joinlogo.png"></a>
+    <a href="#" class="nav-element">Board</a>
+    <a href="#" class="nav-element">Backlog</a>
+    <a href="#" class="nav-element">Add Task</a>
+    <a href="#" class="nav-element">Help</a>
+    <img class="nav-pp" src="./img/pp.jpg">
+    `;
+}
