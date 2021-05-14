573f00d9bc4a28fd1b7a4ba7b750efbb07a67266
Mikail Savas <savasmikail10@gmail.com>
2021-05-14 21:58:23 +0200

    addTask.js: createTask() & deleted addTaskToDatabase()

--- script/addTask.js
+++ script/addTask.js
@@ -1,6 +1,21 @@
-async function addTaskToDatabase(task) {
-    console.log('Erstelle den Task: ', task);
-    let savedTasks = await backend.getItem('backlog') || [];
-    savedTasks.push(task);
-    backend.setItem('backlog', savedTasks)
+function addTask() {
+
+    let title = document.getElementById('title').value;
+    let category = document.getElementById('category').value;
+    let description = document.getElementById('description').value;
+    let dueDate = document.getElementById('dueDate').value;
+    let urgency = document.getElementById('urgency').value;
+
+    let task = {
+        'title': title,
+        'category': category,
+        'description': description,
+        'createdAt': new Date().getTime(),
+        'dueDate': dueDate,
+        'urgency': urgency,
+        'board-category': '',
+        'assignedTo': ''
+    }
+
+    addObjectToDatabase('backlogTasks', task);
 }
